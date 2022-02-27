3a9811e3bee10aebb4f253ce07697293f64b3583
Honza Sládek <me@honzasladek.com>
2022-02-27 14:41:48 +0100

    Add new logo + change favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,7 @@ const Home: NextPage = ({offerTypes, districts}: any) => {
 			<Header />
 			<div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
 				<div className="text-center">
-					<p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-						Pomáhej Ukrajině
-					</p>
+					<Image src="/pomahej-ukrajine.png" width={434} height={64} alt="Pomáhej Ukrajině" />
 					<p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
 						Neziskové organizace pracující s migranty v ČR se spojily a toto je centrální místo, kde můžete nabídnout svou pomoc. Některé nabídky budou přímo zveřejněny a mohou na ně reagovat ti, kdo pomoc potřebují. Ostatní nabídky budou zpracovány kolegy z místních neziskových organizací nebo obcí. Kolegové se vám ozvou a domluvíte se na konkrétní spolupráci.
 					</p>
