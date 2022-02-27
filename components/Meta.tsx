3196efda91ecc8cb6f3ce2906ac5729db444b77c
Honza Sládek <me@honzasladek.com>
2022-02-27 12:16:00 +0100

    Correct favicons

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -23,27 +23,22 @@ const Meta = (props: IMetaProps) => {
         />
         <link
           rel="apple-touch-icon"
-          href={`${router.basePath}/apple-touch-icon.png`}
+          href={`/apple-touch-icon.png`}
           key="apple"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href={`${router.basePath}/favicon-32x32.png`}
+          href={`/favicon-32x32.png`}
           key="icon32"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href={`${router.basePath}/favicon-16x16.png`}
+          href={`/favicon-16x16.png`}
           key="icon16"
-        />
-        <link
-          rel="icon"
-          href={`${router.basePath}/favicon.ico`}
-          key="favicon"
         />
       </Head>
       <NextSeo
