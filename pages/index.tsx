cda5f0ff5a5ba0632198ccfadf1abcf2c3e6c37e
Honza Sládek <me@honzasladek.com>
2022-03-01 11:18:31 +0100

    Add email to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,9 @@ const Home: NextPage = ({offerTypes, districts}: any) => {
 					<p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
 						Neziskové organizace pracující s migranty v ČR se spojily a toto je centrální místo, kde můžete nabídnout svou pomoc. Některé nabídky budou přímo zveřejněny a mohou na ně reagovat ti, kdo pomoc potřebují. Ostatní nabídky budou zpracovány kolegy z místních neziskových organizací nebo obcí. Kolegové se vám ozvou a domluvíte se na konkrétní spolupráci.
 					</p>
+					<p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+						Pokud nás potřebujete kontaktovat, napište na <a className='underline underline-offset-2' href="mailto:pomahejukrajine@migracnikonscorcium.cz?subject=PomáhejUkrajině">pomahejukrajine@migracnikonscorcium.cz</a>.
+					</p>
 					<div className={`mt-10 space-x-4 ${process.env.NEXT_TEMPORARY == 'TEMPORARY' ? 'hidden' : ''}`}>
 						<Link href="/nabidka">
 							<a
