009877cf09124b3ef939c027cd00e62105d90be3
Jiri Vyhnalek <jiri@vyhnalek.net>
2022-03-24 22:14:40 +0100

    [#7] Required phone number in reg. form

--- lib/shared.ts
+++ lib/shared.ts
@@ -1,90 +1,103 @@
-
 export interface QuestionDefinition {
-	id: string
-	question: string
-	type: QuestionType
-	required: boolean
+	id: string;
+	question: string;
+	type: QuestionType;
+	required: boolean;
 	options: {
-		id: string
-		value: string
-		label: string
-		requireSpecification: boolean
-	}[]
+		id: string;
+		value: string;
+		label: string;
+		requireSpecification: boolean;
+	}[];
 }
 
-export type QuestionType = 'radio' | 'checkbox' | 'text' | 'textarea' | 'number' | 'date' | 'district'
+export type QuestionType =
+	| "radio"
+	| "checkbox"
+	| "text"
+	| "textarea"
+	| "number"
+	| "date"
+	| "district";
 
 export interface QuestionValue {
-	value?: string
-	specification?: string
+	value?: string;
+	specification?: string;
 	values?: {
-		value: string
-		specification?: string
-	}[]
+		value: string;
+		specification?: string;
+	}[];
 }
 
-
 export type Districts = {
-	id: string
-	name: string
+	id: string;
+	name: string;
 	region: {
-		id: string
-		name: string
-	}
-}[]
+		id: string;
+		name: string;
+	};
+}[];
 
 export type Languages = {
-	id: string
-	name: string
-}[]
+	id: string;
+	name: string;
+}[];
 
 export type OfferType = {
-	id: string
-	name: string
-	nameUK: string
-	infoText: string
-	questions: QuestionDefinition[]
-	needsVerification: boolean
-	hideInDemand: boolean
+	id: string;
+	name: string;
+	nameUK: string;
+	infoText: string;
+	questions: QuestionDefinition[];
+	needsVerification: boolean;
+	hideInDemand: boolean;
 };
 
 export interface PublicQueryResult {
-	offerTypes: OfferType[]
-	districts: Districts
-	languages: Languages
-	uk?: boolean
+	offerTypes: OfferType[];
+	districts: Districts;
+	languages: Languages;
+	uk?: boolean;
 }
 
 export type OfferParameters = {
-	[id: string]: QuestionValue
+	[id: string]: QuestionValue;
 };
 
 export interface RegisterFormState {
-	name: string
-	organization: string,
-	phone: string
-	email: string
-	contactHours: string,
-	expertise: string
-	languages: string[],
+	name: string;
+	organization: string;
+	phone: string;
+	email: string;
+	contactHours: string;
+	expertise: string;
+	languages: string[];
 	offers: {
 		[id: string]: {
-			questions: OfferParameters
-		}
-	}
+			questions: OfferParameters;
+		};
+	};
 }
 
 export interface HelpFormState {
-	name: string
-	phone: string
-	email: string
-	contactHours: string,
-	otherType: string,
-	types: string[]
+	name: string;
+	phone: string;
+	email: string;
+	contactHours: string;
+	otherType: string;
+	types: string[];
 }
 
-export type Error = { input: 'question'; questionId: string; message: string } | { input: 'email'; message: string } | { input: 'offer'; message: string } | { input: 'languages'; message: string }
-export type ErrorMultilingual = { input: 'email'; message: {cs: string, uk: string} } | { input: 'offer'; message: {cs: string, uk: string} }
+//FIXME https://github.com/cesko-digital/pomahejukrajine-web/issues/19
+export type Error =
+	| { input: "question"; questionId: string; message: string }
+	| { input: "email"; message: string }
+	| { input: "offer"; message: string }
+	| { input: "languages"; message: string }
+	| { input: "phone"; message: string };
+export type ErrorMultilingual =
+	| { input: "email"; message: { cs: string; uk: string } }
+	| { input: "offer"; message: { cs: string; uk: string } };
 
 export const publicQuery = `
 	offerTypes: listOfferType(orderBy: [{ order: asc }]) {
@@ -123,4 +136,4 @@ export const publicQuery = `
 			name
 		}
 	}
-`
+`;
