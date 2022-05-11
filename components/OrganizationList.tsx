bcd7bf988306222862df2d0adfba6c3ccc91f8f1
Viliam Valent <viliam@valent.email>
2022-05-11 06:29:17 +0200

    Search in organizations list

--- components/OrganizationList.tsx
+++ components/OrganizationList.tsx
@@ -1,23 +1,64 @@
 import React from "react";
 import { Organization } from "../lib/model/Organization";
+import { useTranslation } from "next-i18next";
+
+const COLUMNS: Array<keyof Organization> = [
+	"name",
+	"address",
+	"identificationNumber",
+	"parentOrganization",
+	"organizationTypeName",
+];
 
 const OrganizationList = ({
 	organizations,
 }: {
 	organizations: Organization[];
 }) => {
+	const { t } = useTranslation();
 	return (
-		<div className="mt-8 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
-			{organizations.map((organization) => {
-				return (
-					<div
-						key={organization.id}
-						className="flex flex-col p-4 m-4 border shadow-md rounded-md"
-					>
-						<h3 className="text-lg font-bold">{organization.name}</h3>
-					</div>
-				);
-			})}
+		<div className="container mx-auto">
+			<div className="mt-8 overflow-x-auto rounded-lg shadow-md md:rounded-xl bg-blue-50">
+				<table className="w-full my-4 border-collapse table-auto md:my-8">
+					<thead>
+						<tr>
+							{COLUMNS.map((column) => (
+								<th
+									key={column}
+									className="p-2 pt-0 text-left border-b border-gray-200 md:pb-3 md:p-4 first:pl-4 md:first:pl-8"
+								>
+									{t(`organizace.${column}`)}
+								</th>
+							))}
+						</tr>
+					</thead>
+					<tbody className="bg-white">
+						{organizations.map((organization) => (
+							<tr key={organization.id}>
+								{COLUMNS.map((column) => (
+									<td
+										className="p-2 border-b border-gray-200 md:p-4 first:pl-4 md:first:pl-8"
+										key={column}
+									>
+										{column === "name" && organization.website ? (
+											<a
+												href={organization.website}
+												className="underline underline-offset-2"
+												target="_blank"
+												rel="noreferrer"
+											>
+												{organization.name}
+											</a>
+										) : (
+											organization[column] || ""
+										)}
+									</td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 };
