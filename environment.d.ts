8052901b0543f42bcdd9ed95b5bf6ed48f1ccad8
Jonáš Nobile <jonas.nobile@icloud.com>
2022-05-13 12:14:49 +0200

    WIP: search

--- environment.d.ts
+++ environment.d.ts
@@ -6,6 +6,10 @@ declare global {
 			NODE_ENV: "development" | "production";
 			PORT?: string;
 			PWD: string;
+			NEXT_PUBLIC_TYPESENSE_SEARCH_ONLY_API_KEY: string;
+			NEXT_PUBLIC_TYPESENSE_PORT: string;
+			NEXT_PUBLIC_TYPESENSE_HOST: string;
+			NEXT_PUBLIC_TYPESENSE_PROTOCOL: string;
 		}
 	}
 }
