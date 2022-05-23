866ae57c54d79dcb9798e952153d3693e2d77e5e
Matej Sveda <msveda@engeto.com>
2022-05-23 08:50:06 +0200

    add text to locale

--- components/EditForm.tsx
+++ components/EditForm.tsx
@@ -99,7 +99,7 @@ export const EditForm = memo<RegisterFormProps>(
 			return (
 				<>
 					<div className="p-2 rounded-lg bg-indigo-600 shadow-lg sm:p-3 text-center text-lg">
-						<p className="mx-3 font-medium text-white">Nabídka byl upravena.</p>
+						<p className="mx-3 font-medium text-white">{t("nabidka.edited")}</p>
 					</div>
 					<div className="flex justify-center mt-3">
 						<Link href="/moje-nabidky">
