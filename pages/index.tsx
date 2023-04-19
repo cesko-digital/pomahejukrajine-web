c9920c347af5f272bb07ddd99d8fc431f55355d3
radomila <hejhalova.radomila@gmail.com>
2023-04-19 16:37:13 +0200

    fix: logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,8 +30,8 @@ const Logos = [
 		href: "https://www.facebook.com/atlasoftodaysworld",
 		path: "/logos/atlasmigrace.jpg",
 	}, 
-	{ href: "https://dignityrh.org/", path: "/logos/dignity.jpg"}, 
-	{ href: "http://www.kalyna.cz/", path: "/logos/kalyna.png"}, 
+	{ href: "https://dignityrh.org/", path: "/logos/dignity.svg"}, 
+	{ href: "http://www.kalyna.cz/", path: "/logos/kalyna.svg"}, 
 	{ href: "https://czechdoc.cz/cs/", path: "/logos/czechdoc.png"}
 ] as const;
 
