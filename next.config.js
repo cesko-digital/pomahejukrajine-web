89f96a36137ed368b23f3b2c00339f6a9d9dec31
hankadev <hana.klingova@gmail.com>
2023-11-22 12:38:56 +0100

    fix: redirect all routes to main page

--- next.config.js
+++ next.config.js
@@ -1,22 +1,27 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
 
-const id = "3a0731d3-2102-4a7b-a5c4-096e0876f10b";
+// const id = "3a0731d3-2102-4a7b-a5c4-096e0876f10b";
 
 const nextConfig = {
 	reactStrictMode: true,
 	async redirects() {
 		return [
 			{
-				source: "/nabidky",
-				destination: `/nabidky/${id}`,
+				source: "/:path+",
+				destination: "/",
 				permanent: true,
 			},
-			{
-				source: "/neverejne-nabidky",
-				destination: `/neverejne-nabidky/${id}`,
-				permanent: true,
-			},
+			// {
+			// 	source: "/nabidky",
+			// 	destination: `/nabidky/${id}`,
+			// 	permanent: true,
+			// },
+			// {
+			// 	source: "/neverejne-nabidky",
+			// 	destination: `/neverejne-nabidky/${id}`,
+			// 	permanent: true,
+			// },
 		];
 	},
 	i18n,
